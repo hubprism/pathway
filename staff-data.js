122d206e1b184e4888f52fb32fb199a81d8462c0
Syvatech <nwoguuchechukwu@gmail.com>
2026-03-09 18:16:43 +0100

    first2

---
+++ staff-data.js
@@ -0,0 +1,25 @@
+const staff = {
+
+1:{
+name:"Uchechukwu Nwogu",
+position:"IT Support Specialist",
+company:"Pathway Holdings Limited",
+website:"https://pathway.ng",
+email:"uchechukwu@pathway.ng",
+phone:"2349039024299",
+photo:"images/Uchechukwu.png",
+vcf:"uchechukwu.vcf"
+},
+
+2:{
+name:"John Doe",
+position:"Network Engineer",
+company:"Pathway Holdings Limited",
+website:"https://pathway.ng",
+email:"john@pathway.ng",
+phone:"2348011111111",
+photo:"images/john.png",
+vcf:"john.vcf"
+}
+
+};
