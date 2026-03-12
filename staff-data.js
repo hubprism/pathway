9a4a141b45f8b339908750ef14b3ed107aa4fb32
Syvatech <nwoguuchechukwu@gmail.com>
2026-03-12 19:21:53 +0100

    all changes done

--- staff-data.js
+++ staff-data.js
@@ -1,137 +1,133 @@
 const staff = {
+  uchechukwu: {
+    name: "Uchechukwu Nwogu",
+    position: "IT Support Specialist",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "uchechukwu@pathway.ng",
+    phone: "2349039024299",
+    photo: "images/Uchechukwu.png",
+    vcf: "uchechukwu.vcf"
+  },
 
-uchechukwu:{
-name:"Uchechukwu Nwogu",
-position:"IT Support Specialist",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"uchechukwu@pathway.ng",
-phone:"2349039024299",
-photo:"images/Uchechukwu.png",
-vcf:"uchechukwu.vcf"
-},
+  taiwo: {
+    name: "Taiwo Akinsade",
+    position: "Senior IT Specialist",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "taiwo@pathway.ng",
+    phone: "2348022485657",
+    photo: "images/taiwo.png",
+    vcf: "taiwo.vcf"
+  },
 
-taiwo:{
-name:"Taiwo Akinsade",
-position:"Senior IT Specialist",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"taiwo@pathway.ng",
-phone:"+2348022485657",
-photo:"images/taiwo.png",
-vcf:"taiwo.vcf"
-},
+  ajibola: {
+    name: "Ajibola Adekoya",
+    position: "IT Support Specialist",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "ajibola@pathway.ng",
+    phone: "2347032028517",
+    photo: "images/ajibola.png",
+    vcf: "ajibola.vcf"
+  },
 
-ajibola:{
-name:"Ajibola Adekoya",
-position:"IT Support Specialist",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"ajibola@pathway.ng",
-phone:"2347032028517",
-photo:"images/ajibola.png",
-vcf:"ajibola.vcf"
-},
+  paul: {
+    name: "Paul Ojo",
+    position: "IT Support Specialist",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "paul@pathway.ng",
+    phone: "2349166816081",
+    photo: "images/paul.png",
+    vcf: "paul.vcf"
+  },
 
-paul:{
-name:"Paul Ojo",
-position:"IT Support Specialist",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"paul@pathway.ng",
-phone:"2349166816081",
-photo:"images/paul.png",
-vcf:"paul.vcf"
-},
+  blessing: {
+    name: "Blessing Okoro",
+    position: "Receptionist",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "blessing@pathway.ng",
+    phone: "2348126996043",
+    photo: "images/blessing.png",
+    vcf: "blessing.vcf"
+  },
 
-blessing:{
-name:"Blessing Okoro",
-position:"Receptionist",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"blessing@pathway.ng",
-phone:"2348126996043",
-photo:"images/blessing.png",
-vcf:"blessing.vcf"
-},
+  osayi: {
+    name: "Osayi Uuzebor",
+    position: "Human Resources Manager",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "osayig@pathway.ng",
+    phone: "2348036157842",
+    photo: "images/Osayi.png",
+    vcf: "osayi.vcf"
+  },
 
-osayi:{
-name:"Osayi Uuzebor",
-position:"Human Resources Manager",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"osayig@pathway.ng",
-phone:"2348036157842",
-photo:"images/Osayi.png",
-vcf:"osayi.vcf"
-},
+  esther: {
+    name: "Esther Aver",
+    position: "Human Resources",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "estherg@pathway.ng",
+    phone: "2348118216229",
+    photo: "images/esther.png",
+    vcf: "esther.vcf"
+  },
 
-esther:{
-name:"Esther Aver",
-position:"Human Resources",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"estherg@pathway.ng",
-phone:"2348118216229",
-photo:"images/esther.png",
-vcf:"esther.vcf"
-},
+  jane: {
+    name: "Jane Edeh",
+    position: "Human Resources",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "jane@pathway.ng",
+    phone: "2348062365159",
+    photo: "images/jane.png",
+    vcf: "jane.vcf"
+  },
 
-jane:{
-name:"Jane Edeh",
-position:"Human Resources",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"jane@pathway.ng",
-phone:"2348062365159",
-photo:"images/jane.png",
-vcf:"jane.vcf"
-},
+  sanmi: {
+    name: "Sanmi Ikuemonisan",
+    position: "Admin Officer",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "sanmi@pathway.ng",
+    phone: "2348166322131",
+    photo: "images/sanmi.png",
+    vcf: "sanmi.vcf"
+  },
 
-sanmi:{
-name:"Sanmi Ikuemonisan",
-position:"Admin Officer",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"sanmi@pathway.ng",
-phone:"2348166322131",
-photo:"images/sanmi.png",
-vcf:"sanmi.vcf"
-},
+  kunle: {
+    name: "Kunle Akanbi",
+    position: "Admin Officer",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "kunle@pathway.ng",
+    phone: "2347062894636",
+    photo: "images/kunle.png",
+    vcf: "kunle.vcf"
+  },
 
-kunle:{
-name:"Kunle Akanbi",
-position:"Admin Officer",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"kunle@pathway.ng",
-phone:"2347062894636",
-photo:"images/kunle.png",
-vcf:"kunle.vcf"
-},
+  adekunle: {
+    name: "Adekunle Alade",
+    position: "GCEO",
+    company: "Pathway Holdings Limited",
+    website: "https://pathwayadvisorsng.com",
+    email: "adekunle@pathwayadvisorsng.com",
+    phone: "2347069721470",
+    photo: "images/adekunle.png",
+    vcf: "adekunle.vcf"
+  },
 
-adekunle:{
-name:"Adekunle Alade",
-position:"GCEO",
-company:"Pathway Holdings Limited",
-website:"https://pathwayadvisorsng.com",
-email:"adekunle@pathwayadvisorsng.com",
-phone:"2347069721470",
-photo:"images/adekunle.png",
-vcf:"adekunle.vcf"
-},
-
-dolapo:{
-name:"Dolapo Akanbi-Alade",
-position:"GCOO",
-company:"Pathway Holdings Limited",
-website:"https://pathway.ng",
-email:"dolapo@pathway.ng",
-phone:"2348039210599",
-photo:"images/dolapo.png",
-vcf:"dolapo.vcf"
-},
-
-
-
+  dolapo: {
+    name: "Dolapo Akanbi-Alade",
+    position: "GCOO",
+    company: "Pathway Holdings Limited",
+    website: "https://pathway.ng",
+    email: "dolapo@pathway.ng",
+    phone: "2348039210599",
+    photo: "images/dolapo.png",
+    vcf: "dolapo.vcf"
+  }
 };
